a8f61e136c260699cad4e9f92471577ddb040340
Joo Kang <jkang@cmfgroup.com>
2023-08-09 12:03:12 -0400

    uncommitted

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -2,7 +2,7 @@ import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import bcrypt from "bcryptjs";
 import { User } from "../../entity/User";
 import { RegisterInput } from "./register/RegisterInput";
-import { isAuth } from "../middleware/isAuth";
+// import { isAuth } from "../middleware/isAuth";
 import { logger } from "../middleware/logger";
 import { sendEmail } from "../utils/sendEmail";
 import { createConfirmationUrl } from "../utils/createConfirmationUrl";
@@ -10,7 +10,7 @@ import { createConfirmationUrl } from "../utils/createConfirmationUrl";
 @Resolver()
 export class RegisterResolver {
   @Query(() => String)
-  @UseMiddleware(isAuth, logger)
+  @UseMiddleware(logger)
   async hello() {
     return "hello world!";
   }
